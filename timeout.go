d30066f42300027fb1bf7cfb7839c4122ba2e748
storezhang <storezhang@gmail.com>
2021-09-26 19:51:12 +0800

    增加重连日志

--- timeout.go
+++ timeout.go
@@ -6,9 +6,9 @@ import (
 
 type timeout struct {
 	// Ping超时
-	Ping time.Duration `default:"10" json:"ping" yaml:"ping" xml:"ping" toml:"ping"`
+	Ping time.Duration `default:"10s" json:"ping" yaml:"ping" xml:"ping" toml:"ping"`
 	// 连接超时
-	Connect time.Duration `default:"30" json:"connect" yaml:"connect" xml:"connect" toml:"connect"`
+	Connect time.Duration `default:"30s" json:"connect" yaml:"connect" xml:"connect" toml:"connect"`
 	// 写入超时
 	Write time.Duration `json:"write" yaml:"write" xml:"write" toml:"write"`
 }
