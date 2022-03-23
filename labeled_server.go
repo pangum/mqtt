b2924838238c1c1bb405e33e33c9993f55586809
storezhang <storezhang@gmail.com>
2022-03-23 10:52:36 +0800

    升级依赖

--- labeled_server.go
+++ labeled_server.go
@@ -1,7 +1,7 @@
 package mqtt
 
 type labeledServer struct {
-	// 标签，后续
+	// 标签，可以通过标签来选择不同的服务器
 	Label string `json:"label" yaml:"label" xml:"label" toml:"label" validate:"required"`
 	// 地址
 	Broker broker `json:"broker" yaml:"broker" xml:"broker" toml:"broker" validate:"required"`
